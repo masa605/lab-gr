9ccb60b9c57f6f8c57119006a24a144c51270359
masa605 <m-morita@yuruyakana.com>
2026-08-19 14:50:44 +0900

    Stripe url 追加した

--- Lab_gr.py
+++ Lab_gr.py
@@ -219,12 +219,12 @@ def main():
     # ----------------------------------------------------
     st.sidebar.markdown("---")
     st.sidebar.subheader("👑 プレミア版へのアップグレード")
-    st.sidebar.captionn("高度なブレンド逆算やデータの保存機能を利用するには、プレミアムプラン（月額500円）への登録が必要です。)
+    st.sidebar.captionn("高度なブレンド逆算やデータの保存機能を利用するには、プレミアムプラン（月額500円）への登録が必要です。")
                         
     # マスターが取得したtest_ から始まるStripeのURLに際変えて下さい
     stripe_url = "https://buy.stripe.com/test_eVq14meRz8nud5t29adZ600"
     
-    st.sidebar.link_button("プレミアム機能を開放する", stripe_payment_link, type="primary")
+    st.sidebar.link_button("プレミアム機能を開放する", stripe_url, type="primary")
     
 
     # ----------------------------------------------------
